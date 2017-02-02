7c66e0aaa3c5a1de8f497e113a4f9617a3ba2462
Jerry <jerry@Jerrys-MacBook-Pro.local>
2017-02-02 13:53:10 +0700

    perbaikan side-panel

--- resources/views/layouts/sidebar/user-panel.blade.php
+++ resources/views/layouts/sidebar/user-panel.blade.php
@@ -4,8 +4,9 @@
             <img src="http://www.gravatar.com/avatar/{{ md5(Auth::user()->email) }}?d=identicon" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
-            <p>{{ Auth::user()->full_name }}</p>
+            <p>{{ Auth::user()->name }}</p>
             <!-- Status -->
-            <a href="{{ url('user/profile') }}"><i class="fa fa-circle text-success"></i> {{ App\User::find(Auth::user()->id)->role->display_name }}</a>
+            <a href="{{ url('user/profile') }}"><i class="fa fa-circle text-success"></i> {{ Auth::user()->rolename }}</a>
           </div>
         </div>
+          
